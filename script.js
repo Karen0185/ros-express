6b56ae2de3366d8183a25c9021bc495414de334c
Karen Harutyunyan <karen@MacBook-Pro-Karen.local>
2023-12-20 07:06:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let burgerToggle = document.querySelector('.burger__icon');
+let header = document.querySelector('header');
+let burgerNav = document.querySelector('.nav__burger');
+
+burgerToggle.addEventListener('click', () => {
+    burgerToggle.classList.contains('active') ? burgerToggle.classList.remove('active') : burgerToggle.classList.add('active')
+    header.classList.contains('fixed') ? header.classList.remove('fixed') : header.classList.add('fixed')
+    burgerNav.classList.contains('open') ? burgerNav.classList.remove('open') : burgerNav.classList.add('open')
+})
